c440514b2585dcf4f7d12c98c47799ac894891ef
Tyler Fedoris <tylerfedoris@outlook.com>
2023-01-26 16:23:13 -0500

    setup cell generation and implement a way to determine connected bubbles

--- Assets/_Project/Code/Scripts/GridCell.cs
+++ Assets/_Project/Code/Scripts/GridCell.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridCell : MonoBehaviour
 {
-    [SerializeField] private Vector2 _gridPosition;
+    public static event Action<Vector2Int> OnBubblePlaced;
+    
+    [SerializeField] private Vector2Int _gridPosition;
     
     [SerializeField] private GameObject _bubble;
 
-    [SerializeField] private GameObject[] _connectedBubbles;
+    [SerializeField] private List<GameObject> _connectedBubbles = new();
 
-    public Vector2 GridPosition
+    public Vector2Int GridPosition
     {
         get => _gridPosition;
         set => _gridPosition = value;
@@ -20,6 +23,12 @@ public class GridCell : MonoBehaviour
     {
         get => _bubble;
         set => _bubble = value;
+    }
+    
+    public List<GameObject> ConnectedBubbles
+    {
+        get => _connectedBubbles;
+        set => _connectedBubbles = value;
     }
 
     // Start is called before the first frame update
@@ -31,6 +40,16 @@ public class GridCell : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void AddConnectedBubble(GameObject bubble)
+    {
+        _connectedBubbles.Add(bubble);
+    }
+
+    public void ClearConnectedBubbles()
+    {
+        _connectedBubbles.Clear();
     }
 }
--- Assets/_Project/Code/Scripts/GridSystem.cs
+++ Assets/_Project/Code/Scripts/GridSystem.cs
@@ -70,30 +70,101 @@ public class GridSystem : MonoBehaviour
             for (var column = 0; column < columnCount; column++)
             {
                 var cell = Instantiate(_cellPrefab, transform);
+                
                 cell.transform.localPosition =
                     new Vector3(column * _cellSize + (Convert.ToInt16(isRowOdd) * (_cellSize / 2f)), row * (-_cellSize + (_cellSize / 10f)),
                         0f);
+                
                 cell.name = "Cell[" + row + ", " + column + "]";
+                
                 _grid[row][column] = cell;
+                
+                var gridCell = cell.GetComponent<GridCell>();
+                gridCell.GridPosition = new Vector2Int(row, column);
+                
+                var randomIndex = UnityEngine.Random.Range(0, _bubblePrefabs.Length);
+                gridCell.Bubble = Instantiate(_bubblePrefabs[randomIndex], cell.transform);
             }
         }
+
+        LinkConnectedBubbles();
     }
 
     private void SetBoundaryPositionAndScale()
     {
-        var leftBoundaryPosition = _boundaries.LeftBoundary.transform.position;
-        _boundaries.LeftBoundary.transform.position = new Vector3(_gridWidth == GridWidth.Narrow ? -2.9f : -4.15f, leftBoundaryPosition.y, leftBoundaryPosition.z);
+        var leftBoundaryTransform = _boundaries.LeftBoundary.transform;
+        var leftBoundaryPosition = leftBoundaryTransform.position;
+        leftBoundaryTransform.position = new Vector3(_gridWidth == GridWidth.Narrow ? -2.9f : -4.15f, leftBoundaryPosition.y, leftBoundaryPosition.z);
         
-        var rightBoundaryPosition = _boundaries.RightBoundary.transform.position;
-        _boundaries.RightBoundary.transform.position = new Vector3(_gridWidth == GridWidth.Narrow ? 2.9f : 4.15f, rightBoundaryPosition.y, rightBoundaryPosition.z);
+        var rightBoundaryTransform = _boundaries.RightBoundary.transform;
+        var rightBoundaryPosition = rightBoundaryTransform.position;
+        rightBoundaryTransform.position = new Vector3(_gridWidth == GridWidth.Narrow ? 2.9f : 4.15f, rightBoundaryPosition.y, rightBoundaryPosition.z);
 
-        var gridPosition = transform.position;
-        transform.position = new Vector3(_boundaries.LeftBoundary.transform.position.x + 0.4f, gridPosition.y, gridPosition.z);
+        var gridTransform = transform;
+        var gridPosition = gridTransform.position;
+        gridTransform.position = new Vector3(leftBoundaryTransform.position.x + 0.4f, gridPosition.y, gridPosition.z);
         
-        var topBoundaryScale = _boundaries.TopBoundary.transform.localScale;
-        _boundaries.TopBoundary.transform.localScale = new Vector3(topBoundaryScale.x,_gridWidth == GridWidth.Narrow ? 6f : 8.5f, topBoundaryScale.z);
+        var topBoundaryTransform = _boundaries.TopBoundary.transform;
+        var topBoundaryScale = topBoundaryTransform.localScale;
+        topBoundaryTransform.localScale = new Vector3(topBoundaryScale.x,_gridWidth == GridWidth.Narrow ? 6f : 8.5f, topBoundaryScale.z);
         
-        var bottomBoundaryScale = _boundaries.BottomBoundary.transform.localScale;
-        _boundaries.BottomBoundary.transform.localScale = new Vector3(bottomBoundaryScale.x,_gridWidth == GridWidth.Narrow ? 6f : 8.5f, bottomBoundaryScale.z);
+        var bottomBoundaryTransform = _boundaries.BottomBoundary.transform;
+        var bottomBoundaryScale = bottomBoundaryTransform.localScale;
+        bottomBoundaryTransform.localScale = new Vector3(bottomBoundaryScale.x,_gridWidth == GridWidth.Narrow ? 6f : 8.5f, bottomBoundaryScale.z);
+    }
+
+    private void LinkConnectedBubbles()
+    {
+        for (var row = 0; row < _grid.Length; row++)
+        {
+            for (var column = 0; column < _grid[row].Length; column++)
+            {
+                var gridCell = _grid[row][column].GetComponent<GridCell>();
+                if (gridCell)
+                {
+                    GetConnectedBubbles(ref gridCell, row, column);
+                }
+            }
+        }
+    }
+
+    private void GetConnectedBubbles(ref GridCell gridCell, int row, int column)
+    {
+        if (!gridCell.Bubble)
+        {
+            return;
+        }
+        
+        gridCell.ClearConnectedBubbles();
+
+        bool isRowEven = row % 2 == 0;
+        var positionsToCheck = new List<Vector2Int>
+        {
+            new(row - 1, column),
+            new(row + 1, column),
+            new(row, column - 1),
+            new(row, column + 1),
+            new(row - 1, isRowEven ? column - 1 : column + 1),
+            new(row + 1, isRowEven ? column - 1 : column + 1),
+        };
+
+        foreach (var position in positionsToCheck)
+        {
+            if (!IsValidGridPosition(position.x, position.y))
+            {
+                continue;
+            }
+            
+            var bubble = _grid[position.x][position.y].GetComponent<GridCell>().Bubble;
+            if (bubble)
+            {
+                gridCell.AddConnectedBubble(bubble);
+            }
+        }
+    }
+
+    private bool IsValidGridPosition(int row, int column)
+    {
+        return row >= 0 && row < _grid.Length && column >= 0 && column < _grid[row].Length;
     }
 }
